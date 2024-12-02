c66b2942dcd5c13b0236779a53649aeaa04c0b34
AvalonHaney <alh200@case.edu>
2024-12-02 03:39:53 -0500

    messed up hosting on render stuff

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import startSocketServer from "./socket.js";
 
 const app = Express();
 const server = http.createServer(app);
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use("/", Express.static("client"));
 server.listen(port, () => console.log("Server running on port " + port));
